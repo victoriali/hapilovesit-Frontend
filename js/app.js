492b7d7024f574ad3781e12ce2f7f41582f14a84
Victoria Li <wingman.li@hotmail.com>
2015-04-22 23:19:50 +0800

    third commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
+  $('.img-zoom').hover(function() {
+  		console.log("hello")
+      $(this).addClass('transition');
 
+  }, function() {
+      $(this).removeClass('transition');
+  });
 });
