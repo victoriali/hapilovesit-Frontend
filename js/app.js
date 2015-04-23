648ab8d54c9e707680cf206845fcee86eef8d2e9
Victoria Li <wingman.li@hotmail.com>
2015-04-23 11:52:35 +0800

    fourth commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,49 @@
 $(document).ready(function(){
-  $('.img-zoom').hover(function() {
-  		console.log("hello")
-      $(this).addClass('transition');
 
-  }, function() {
-      $(this).removeClass('transition');
-  });
+//-------------- Signup && SignIn && Authenticate Redirect-----------------
+	$('#signUpButton').on("click", function(){
+		$.ajax({
+	    type: "POST",
+	    url: "http://localhost:3000/users",
+	    dataType: 'json',
+	    data: {
+        user: {
+          username: $('#signUpUsername').val(),
+          email: $('#signUpUserEmail').val(),
+          password: $('#signUpUserPassword').val()
+        }
+      },
+      xhrFields: {
+    		withCredentials: true
+ 			},
+	    success: function(response){
+	    	if (response.Error==="Username already exist"){
+	    		$('#warningAlreadySignUp').show("slow");
+	    		$('#warningAlreadySignUp').text('Hello '+$('#signUpUsername').val()+', you have signed up already, want to signin?')
+	    	}
+      	console.log(response);
+
+      	//and also sign in using same username
+	    	$.ajax({
+			    type: "POST",
+			    url: "http://localhost:3000/sessions",
+			    dataType: 'json',
+			    data: {
+		        user: {
+		          username: $('#signUpUsername').val(),
+		          password: $('#signUpUserPassword').val()
+		        }
+		      },
+		      xhrFields: {
+	    			withCredentials: true
+	  	    },
+			    success: function(response){
+		      	console.log(response);
+		      	console.log('signedin');
+		      	
+		  		}
+				});
+	    }
+		});
+	});
 });
--- js/requests.js
+++ js/requests.js
@@ -1,4 +1,85 @@
-$(document).ready(function(){
+
+//---------Signup User------------
+  function signupUser(username, email, password, callback) {
+    $.ajax({
+	    type: "POST",
+	    url: "http://localhost:3000/users",
+	    dataType: 'json',
+	    data: {
+        user: {
+          username: username,
+          email: email,
+          password: password
+        }
+      },
+      xhrFields: {
+    		withCredentials: true
+ 			},
+	    success: function(response){
+      	console.log(response);
 
 
-});
+      	return callback(response);
+	    }
+		});
+	};
+
+//---------Signin User------------
+	function signinUser(username, password, callback) {
+    $.ajax({
+	    type: "POST",
+	    url: "http://localhost:3000/sessions",
+	    dataType: 'json',
+	    data: {
+        user: {
+          username: username,
+          password: password
+        }
+      },
+      xhrFields: {
+  			withCredentials: true
+	    },
+	    success: function(response){
+      	console.log(response);
+      	return callback(); 
+			}
+		});
+	};
+
+//--------Authenticate-----------
+	function authenticate(callback) {
+		$.ajax({
+	    type: "GET",
+	    url: "http://localhost:3000/authenticated",
+      xhrFields: {
+  			withCredentials: true
+	    },
+	    success: function(response){
+      	console.log(response);
+      	return callback(response); 
+			}
+		});
+	};
+
+//--------Signout User------------
+	function signoutUser(callback){
+		$.ajax({
+	    type: "DELETE",
+	    url: "http://localhost:3000/sessions",
+      xhrFields: {
+  			withCredentials: true
+	    },
+	    success: function(response){
+	      	console.log(response);
+	      	return callback();
+	    }
+		});
+	}
+//--------Post Orders------------
+	// function postOrder(){
+
+	// }
+//--------Get Orders-------------
+	// function getOrder(){
+
+	// }
