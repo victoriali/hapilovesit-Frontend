b3d2ca0bb212c06e6c8895d2c18b65e593d67574
Victoria Li <wingman.li@hotmail.com>
2015-05-03 18:05:39 +0800

    fixed subscribe page

--- js/cal.js
+++ js/cal.js
@@ -1,11 +1,11 @@
 $(document).on('scroll', function(){
+
   var scroll = $(this).scrollTop();
   if (scroll >= 200) {
-    // $(".navigatorTop").removeClass('navbar-transparent');
     $(".navigatorTop").addClass('navbar-solid');
   };
   if (scroll <= 200) {
-    // $(".navigatorTop").addClass('navbar-transparent');
     $(".navigatorTop").removeClass('navbar-solid');
   };
+
 });
--- js/user.js
+++ js/user.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
   });
 
 
-	$('.hotBotton').on("click", function(){
+	$('.btn-add').on("click", function(){
     var item = $(this).attr("id");
     var price = $(this).attr("value");
 
@@ -91,7 +91,7 @@ function printingRow(){
 	//_____________END OF ADD ITEMS TO SHOPPING CART------
 
 	//-------------GET ALL ORDERS FOR USER------------
-    $.ajax({
+  $.ajax({
     type: "GET",
     url: "http://localhost:3000/orders",
     xhrFields: {
@@ -106,63 +106,102 @@ function printingRow(){
 
         for(var n=0; n<response.length;n++){
           var order = '<div class="col-xs-1">'+
-            n+ //response[n]['_id']+
+            n+ 
             '</div><div class="col-xs-4">'
 
-            var totalPrice = 0;
-            for (var i = 0; i < response[n].items.length; i++){
-              var item = response[n].items[i].item;
-              var quantity = response[n].items[i].quantity;
-              var price = response[n].items[i].price;
-              order += '<div class="col-xs-12">'+quantity+' x '+item+'</div>'
-              totalPrice += price * quantity;
-            }
+          var totalPrice = 0;
+          for (var i = 0; i < response[n].items.length; i++){
+            var item = response[n].items[i].item;
+            var quantity = response[n].items[i].quantity;
+            var price = response[n].items[i].price;
+            order += '<div class="col-xs-12">'+quantity+' x '+item+'</div>'
+            totalPrice += price * quantity;
+          }
 
-            order += '</div><div class="col-xs-2">'+
-              totalPrice+
-              '</div><div class="col-xs-5">'+
-              response[n].orderTime+
-              '</div>'
-            $(order).appendTo('.orderSummary');
+          order += '</div><div class="col-xs-2">'+
+            totalPrice+
+            '</div><div class="col-xs-5">'+
+            response[n].orderTime+
+            '</div>'
+          $(order).appendTo('.orderSummary');
         }  
-
       }
     }
   });
 
 //-------------POST ORDER FOR USER------------
-		
-		var rows = $('#shoppingTable tbody tr');
+    
+  $(document).on("click", '.submitOrder',function(){
+
+    var rows = $('#shoppingTable tbody tr');
     var items = [];
-    for(var i = 0; i < $('#shoppingTable tbody tr').length - 1; i++){
+    for(var i = 0; i < $('#shoppingTable tbody tr').length; i++){
       var row = rows[i];
       var item = $($($(row).find('td')[0])).html();
       var price = $($($(row).find('td')[1])).html();
       var quantity = $($($(row).find('td')[2])).html();
       items.push({item: item, price: price, quantity: quantity});
     }
+    console.log(items);
 
-		$.ajax({
-	    type: "POST",
-	    url: "http://localhost:3000/orders",
+  	$.ajax({
+      type: "POST",
+      url: "http://localhost:3000/orders",
       xhrFields: {
       withCredentials: true
       },
-	    data: {
+      data: {
         order: {
           'items': items,
-          
           shippingMethod: $('input[name=optradio]:checked').attr('value'),
           orderTime: Date()
         }
       },
-	    dataType: 'json',
-	    success: function(response){
+      dataType: 'json',
+      success: function(response){
         console.log("success", response);
+        //-------------GET ALL ORDER FOR USER--------------//
+        $.ajax({
+          type: "GET",
+          url: "http://localhost:3000/orders",
+          xhrFields: {
+            withCredentials: true
+          },
+          success: function(response){
+            if(response.length===0){
+              $('#cd-bg-5').show('slow');
+            }
+            if(response.length>0){
+              $('.noOrder').hide();
 
-	    }
-	  });
-	});
-	//-------------END OF POST ORDER FOR USER------------
+              for(var n=0; n<response.length;n++){
+                var order = '<div class="col-xs-1">'+
+                  n+ //response[n]['_id']+
+                  '</div><div class="col-xs-4">'
+
+                var totalPrice = 0;
+                for (var i = 0; i < response[n].items.length; i++){
+                  var item = response[n].items[i].item;
+                  var quantity = response[n].items[i].quantity;
+                  var price = response[n].items[i].price;
+                  order += '<div class="col-xs-12">'+quantity+' x '+item+'</div>'
+                  totalPrice += price * quantity;
+                }
+
+                order += '</div><div class="col-xs-2">'+
+                  totalPrice+
+                  '</div><div class="col-xs-5">'+
+                  response[n].orderTime+
+                  '</div>'
+                $(order).appendTo('.orderSummary');
+              }  
+            }
+          }
+        });
+        //-------------END OF GET ALL ORDER FOR USER---------//
+      }
+    });
+  });
+	//-------------END OF POST ORDER FOR USER------------//
 
 });
